f515a36773d84d2f8bbb3733a0dc19338f7b64f1
Varun-poluka <poluka.var4126@gmail.com>
2025-02-17 02:02:11 +0530

    labsheet-3

--- labsheet-3/myswap.c
+++ labsheet-3/myswap.c
@@ -2,12 +2,12 @@
 
 int main()
 {
-    char char1, char2, temp;
+    char char1, char2, temp; 
     printf("what are char1 and char2? ");
     scanf("%c%c", &char1, &char2);
 
     temp = char1;
-    char1 = char2;
+    char1 = char2;//interchaning charecters
     char2 = temp;
 
     printf("%c%c\n", char1, char2);
