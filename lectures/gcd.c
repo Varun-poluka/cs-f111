bb6c286014453b54f05bc6270a9cc61cdc3a7ed0
Varun-poluka <poluka.var4126@gmail.com>
2025-02-18 10:17:17 +0530

    comments

--- lectures/gcd.c
+++ lectures/gcd.c
@@ -11,7 +11,7 @@ int main()
        r0= m;
        r1= a;
     }
-    else
+    else // assigning values of r0 and r1 based on which number is larger
     {
         r0= a;
         r1= m;
@@ -20,12 +20,12 @@ int main()
     {
         printf("GCD of %d and %d is %d", a, m, r0);
     }
-    r2 = r0 % r1;
+    r2 = r0 % r1; //finding the reminder by dividing the given numbers 
     while(r2!=0)
     {
         r0 = r1;
-        r1 = r2;
-        r2 = r0 % r1;
+        r1 = r2; //updating the values
+        r2 = r0 % r1; // repeating the division untill r2 becomes 0
     }
     printf("GCD of %d and %d is %d\n", a, m, r1);
 }
