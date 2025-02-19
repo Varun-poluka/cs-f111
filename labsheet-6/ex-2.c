00b2b9bf418dd6ea8bc0cba7812d65abd18ce695
Varun-poluka <poluka.var4126@gmail.com>
2025-02-20 03:05:46 +0530

    labsheet-6

--- labsheet-6/ex-2.c
+++ labsheet-6/ex-2.c
@@ -23,5 +23,5 @@ int main()
         
     }
     printf("%d\n", sum);
-
+    
 }
---
+++ labsheet-6/ex-5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int sum=0;
+    int a;
+    int i;
+    printf("enter the number ");
+    scanf("%d", &n);
+    i = n;
+
+    while(i>0)
+    {
+        a = i%10;
+        sum = sum+(a*a*a);
+        i = i/10;
+    }
+    if(sum == n)
+    {
+        printf("it is an armstrong number\n");
+    }
+    else
+    {
+        printf("it is not a armstrong number\n");
+    }
+}
