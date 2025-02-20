01268ceb2a25ec9c92ec87a827b848886c5cec24
Varun-poluka <poluka.var4126@gmail.com>
2025-02-21 03:06:39 +0530

    labsheet-5

--- labsheet-5/ex-1.c
+++ labsheet-5/ex-1.c
@@ -2,5 +2,22 @@
 
 int main()
 {
-    
+    int n, b=0, c;
+    printf("enter the number ");
+    scanf("%d", &n);
+    c = n;
+
+    for(int i=n; i>0; i=i/10)
+    {
+        int a = i%10;
+        b = (b*10)+a;
+    }
+    if(b==c)
+    {
+        printf("it is an palindrome\n");
+    }
+    else
+    {
+        printf("it is not a palindrome\n");
+    }
 }
