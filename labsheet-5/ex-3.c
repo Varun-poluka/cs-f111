11a79e3e370c22bed245f477db1ba79bd05bc556
Varun-poluka <poluka.var4126@gmail.com>
2025-02-22 02:05:17 +0530

    labsheet-5

--- labsheet-5/ex-3.c
+++ labsheet-5/ex-3.c
@@ -2,7 +2,17 @@
 
 int main()
 {
-    double n;
-    printf("enter the number ");
-    scanf("%lf", &n);
+    int decimal;
+    long int binary=0,place=1;
+    printf("enter the decimal number ");
+    scanf("%d", &decimal);
+
+    for(int i=decimal;i>0;i=i/2)
+    {
+        int reminder = decimal%2;
+        binary = binary +reminder*place;
+        decimal = decimal/2;
+        place = place*10;
+    }
+    printf("%ld\n", binary );
 }
