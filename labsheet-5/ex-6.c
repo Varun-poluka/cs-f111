a0071c7bfa3ecb6a05e455cdafa244c835434b1d
Varun-poluka <poluka.var4126@gmail.com>
2025-02-22 02:01:14 +0530

    labsheet-5

---
+++ labsheet-5/ex-6.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, num=2,count=0,prime;
+    printf("enter the value of n ");
+    scanf("%d", &n);
+
+    while(count<n)
+    {
+        prime = 1;
+        for(int i=2;i*i<=num;i++)
+        {
+            if(num%i==0)
+            {
+                prime = 0;
+                break;
+            }
+        }
+        if(prime==1 )
+        {
+            printf("%d ",num);
+            count++;
+        }
+        num++;
+    }
+    printf("\n");
+}
