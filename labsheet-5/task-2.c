637de71d18a07c60439f665636ba3669c9309d27
Varun-poluka <poluka.var4126@gmail.com>
2025-02-21 15:38:15 +0530

    labsheet-5

--- labsheet-5/task-2.c
+++ labsheet-5/task-2.c
@@ -1,2 +1,23 @@
 #include <stdio.h>
 
+int main()
+{
+    int prime;
+
+    for(int i=2;i<100;i++)
+    {
+        prime = 1;
+        for(int j=2;j*j<=i;j++)
+        {
+            if(i%j==0)
+            {
+                prime = 0;
+                break;
+            }
+        }
+        if(prime==1)
+        {
+            printf("%d is a prime number\n", i);
+        }
+    }
+}
