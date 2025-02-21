34a9ce88b5ee2aa4bf9a3a545971d9567cd9aa6f
Varun-poluka <poluka.var4126@gmail.com>
2025-02-21 08:45:21 +0530

    lectures

---
+++ lectures/case.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+    printf("enter the alphabet ");
+    scanf("%c", &ch);
+
+    if(ch>='a' && ch<='z')
+    {
+        ch = ch-('a'-'A');
+        printf("%c\n", ch);
+    }
+    else if(ch>='A' && ch<='Z')
+    {
+        ch = ch+('a'-'A');
+        printf("%c\n", ch);
+    }
+    else
+    {
+        printf("not a alphabet enetered\n");
+    }
+}    
---
+++ lectures/hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i=1;
+    printf("enter how many times ");
+    scanf("%d", &n);
+
+    while(i<=n)
+    {
+        printf("Hello\n");
+        i++;
+    }
+
+}
