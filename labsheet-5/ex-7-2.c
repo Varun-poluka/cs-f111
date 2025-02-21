9a18414fc8faeb7f4bb1e3a710b388616603983c
Varun-poluka <poluka.var4126@gmail.com>
2025-02-22 02:31:53 +0530

    labsheet-5

---
+++ labsheet-5/ex-7-2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int i;
+    printf("enter the number of rows ");
+    scanf("%d", &n);
+
+    for(int j=1; j<=n; j++)
+    {
+        for(int k=1; k<=j; k++)
+        {
+            printf("%d ", i);
+            i++;
+        }
+        printf("\n");
+    }
+}
