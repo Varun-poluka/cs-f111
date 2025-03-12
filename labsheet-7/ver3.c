0b2bb6706f8b010025277cd5b8ca5b040f8b8d6d
Varun-poluka <poluka.var4126@gmail.com>
2025-03-13 00:30:31 +0530

    comments

--- labsheet-7/ver3.c
+++ labsheet-7/ver3.c
@@ -6,13 +6,13 @@ int main()
     float average;
     for(int i=0;i<100;i++)
     {
-        scanf("%d",&marks[i]);
+        scanf("%d",&marks[i]);//take input from input.txt file 
     }
     for(int i=0;i<100;i++)
     {
         sum = sum+marks[i];
     }
-    average = (float)sum/100;
+    average = (float)sum/100;//explicit typecasting 
     printf("%f\n", average);
     return 0;
 }
