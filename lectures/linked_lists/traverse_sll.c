af84d45214cfe5f2a7bdd41a4cfa2f527e5cbc8f
Varun-poluka <poluka.var4126@gmail.com>
2025-04-25 19:05:17 +0530

    travesing a single linked list

---
+++ lectures/linked_lists/traverse_sll.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+//first creating a single linked list
+struct node
+{
+    int data;
+    struct node *link;
+};
+void nodes(struct node* head);
+int main()
+{
+    struct node *head = (struct node*)malloc(sizeof(struct node));
+    head -> data = 45;
+    head -> link = NULL;
+    struct node *current = (struct node*)malloc(sizeof(struct node));
+    current -> data = 98;
+    current -> link = NULL;
+    head -> link = current;
+    current = (struct node*)malloc(sizeof(struct node));
+    current -> data = 90;
+    current -> link = NULL;
+    head -> link -> link = current;
+    nodes(head);//calling the function to count the node
+}
+void nodes(struct node* head)
+{
+    int count=0;
+    if(head == NULL)
+    {
+        printf("linked list is empty\n");
+        return;
+    }
+    struct node *ptr = head;//storing the addresses of head into another pointer to not change head and loose first node
+    while(ptr!=NULL)//checking if address in the node is NULL
+    {
+        count++;//if its not NULL it is a node so incresing the count
+        ptr = ptr -> link;//updating pointer to go to the next node 
+    }
+    printf("the linked list has %d nodes\n", count);
+}
